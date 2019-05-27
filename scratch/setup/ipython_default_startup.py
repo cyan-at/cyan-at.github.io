2a4e9b8817eacfb306ed18169697ad26986c3303
delilia <cyan@canvas.build>
2019-05-26 23:38:39 -0700

    ipython_default_startup, zeus
    Former-commit-id: bf60de8a9fdf5fcf7d8f103137fe96638a7ab2c5

--- scratch/setup/ipython_default_startup.py
+++ scratch/setup/ipython_default_startup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+# USAGE: symlink wherever this file lives to:
+# ~/.ipython/profile_default/startup/ipython_default_startup.py
+
 import numpy as np
 np.set_printoptions(precision=3)
 np.set_printoptions(suppress=True)
 np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
-print "hello from jim's ipython_default_startup.py"
+print "hello from your custom ipython_default_startup.py"
 
-from wall_borg.interaction.utils import *
-from wall_borg import dynamics as dyn
+import math
+import cPickle as pickle
