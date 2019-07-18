33fecb7896a99dba0207e6e8444d0abd8e0387b2
cyan <jamaisyan18@gmail.com>
2019-07-17 23:35:38 -0700

    updated gif
    Former-commit-id: f502308bf4c8bb6b89e7e8d485f0b103250eb8e1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".sidefinding_counting_facing_0_desc").click(function(){
-    $("#sidefinding_counting_facing_0").toggle();
+  $(".sidefinding_0_desc").click(function(){
+    $("#sidefinding_0").toggle();
   });
 });
