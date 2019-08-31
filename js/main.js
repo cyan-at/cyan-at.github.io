fcb45e581c1a11940978e9010381d6d0fda6e091
cyan <jamaisyan18@gmail.com>
2019-08-30 21:37:10 -0700

    code syntax highlighting
    Former-commit-id: e0ad54569478e51e852b241ba0b430ee457dcc65

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   $(".sidefinding_0_desc").click(function(){
     $("#sidefinding_0").toggle();
   });
+
+  $("#event_dispatch_class_toggle").click(function(){
+    $("#event_dispatch_example").toggle();
+  });
+
+  $("#event_class_toggle").click(function(){
+    $("#event_example").toggle();
+  });
 });
